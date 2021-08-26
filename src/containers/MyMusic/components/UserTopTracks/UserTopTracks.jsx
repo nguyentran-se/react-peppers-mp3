0bec9f53ffc12f3daaeacfd0632e8f2263e6666f
nguyentran-se <nguyentranfw@gmail.com>
2021-08-26 09:48:16 +0700

    add user top track page

--- src/containers/MyMusic/components/UserTopTracks/UserTopTracks.jsx
+++ src/containers/MyMusic/components/UserTopTracks/UserTopTracks.jsx
@@ -1,17 +1,34 @@
 import userApi from "api/userApi";
+import PlaylistSongs from "containers/Playlist/components/PlaylistSongs/PlaylistSongs";
 import React from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 
 const UserTopTracks = () => {
+   const [topTracks, setTopTracks] = useState();
+
    useEffect(() => {
       const requestGetTopTracks = async () => {
-         const params = {};
-         const data = await userApi.getTopTracks();
-         console.log(data);
+         const params = {
+            limit: 50,
+            offset: 0,
+         };
+         const data = await userApi.getTopTracks(params);
+         const updatedTopTracks = {
+            items: data.items,
+            next: data.next,
+            previous: data.previous,
+         };
+         console.log(updatedTopTracks);
+         setTopTracks(updatedTopTracks);
       };
       requestGetTopTracks();
    }, []);
-   return <div className="user-top"></div>;
+   return (
+      <div className="user-top">
+         <PlaylistSongs songs={topTracks?.items} />
+      </div>
+   );
 };
 
 export default UserTopTracks;
