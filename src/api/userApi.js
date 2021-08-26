3f4f4774f6487faa0bc5a7c12584da73650ac999
nguyentran-se <nguyentranfw@gmail.com>
2021-08-26 09:02:52 +0700

    add user favourite songs

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -5,21 +5,30 @@ const userApi = {
       const url = `/me`;
       return axiosClient.get(url);
    },
+
    getTopTracks: (params) => {
       const url = `/me/top/tracks`;
       return axiosClient.get(url, { params });
    },
+
    getTopArtists: (params) => {
       const url = `/me/top/artists`;
       return axiosClient.get(url, { params });
    },
+
    getFollowedArtists: (params) => {
       const url = `/me/following`;
       return axiosClient.get(url, { params });
    },
+
    getUserPlaylists: () => {
       const url = `/me/playlists`;
       return axiosClient.get(url);
+   },
+
+   getFavouriteTracks: (params) => {
+      const url = `/me/tracks`;
+      return axiosClient.get(url, { params });
    },
 };
 
--- src/containers/MyMusic/components/Favourite/Favourite.jsx
+++ src/containers/MyMusic/components/Favourite/Favourite.jsx
@@ -1,7 +1,29 @@
+import userApi from "api/userApi";
+import PlaylistSongs from "containers/Playlist/components/PlaylistSongs/PlaylistSongs";
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Favourite = () => {
-   return <div>user Favourite</div>;
+   const [favouriteSongs, setFavouriteSongs] = useState();
+   useEffect(() => {
+      const requestGetFavouriteTracks = async () => {
+         const favourite = await userApi.getFavouriteTracks();
+         console.log(favourite);
+         const updatedFavourite = {
+            items: favourite.items,
+            next: favourite.next,
+            previous: favourite.previous,
+         };
+         setFavouriteSongs(updatedFavourite);
+      };
+      requestGetFavouriteTracks();
+   }, []);
+   return (
+      <div className="user-favourite">
+         <PlaylistSongs songs={favouriteSongs?.items} />
+      </div>
+   );
 };
 
 export default Favourite;
