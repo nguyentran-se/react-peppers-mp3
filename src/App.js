2b7b8601603b7371c5817ece8af6a8089ddf200b
Peppers <nguyentranfw@gmail.com>
2022-03-14 00:27:43 +0700

    fix: refresh token for both user

--- src/App.js
+++ src/App.js
@@ -14,146 +14,148 @@ import { useHistory } from "react-router-dom";
 import routes from "routes/routes";
 import { selectIsLoggedIn } from "selectors";
 import {
-   loginFail,
-   loginSuccess,
-   requestUserDataFail,
-   requestUserDataSuccess,
+  loginFail,
+  loginSuccess,
+  requestUserDataFail,
+  requestUserDataSuccess,
 } from "store/actions";
-import { requestGetTokenOnExpired } from "store/actions/authAction";
 
 const App = () => {
-   const dispatch = useDispatch();
-   const isLoggedIn = useSelector(selectIsLoggedIn);
-   const history = useHistory();
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const history = useHistory();
 
-   useScrollTop();
+  useScrollTop();
 
-   /**
-    * @implements client_crediential to get data when user not login
-    * @deprecated move into config axios to get token directly. Because of
-    * getLocalstorage is invalid in first load app
-    */
-   // useEffect(() => {
-   //    const requestGetClientCredential = async () => {
-   //       try {
-   //          const response = await credentialApi.getClientCredential();
-   //          // console.log(response?.data.access_token);
-   //          setLocalStorage(PEPPERS, camelize(response?.data));
-   //       } catch (error) {
-   //          console.log(error);
-   //       }
-   //    };
-   //    requestGetClientCredential();
-   // }, []);
+  /**
+   * @implements client_crediential to get data when user not login
+   * @deprecated move into config axios to get token directly. Because of
+   * getLocalstorage is invalid in first load app
+   */
+  // useEffect(() => {
+  //    const requestGetClientCredential = async () => {
+  //       try {
+  //          const response = await credentialApi.getClientCredential();
+  //          // console.log(response?.data.access_token);
+  //          setLocalStorage(PEPPERS, camelize(response?.data));
+  //       } catch (error) {
+  //          console.log(error);
+  //       }
+  //    };
+  //    requestGetClientCredential();
+  // }, []);
 
-   /**
-    * @implements call api refreshToken
-    * @description check USER in localStore to get time expire.
-    */
-   useEffect(() => {
-      let timeout;
-      if (getLocalStorage(USER)) {
-         const {
-            accessToken: token,
-            refreshToken,
-            expiresIn: expiresInTime,
-         } = getLocalStorage(USER);
+  /**
+   * @implements call api refreshToken
+   * @description check USER in localStore to get time expire.
+   * @deprecated move into axios interceptor
+   */
+  // useEffect(() => {
+  //    let timeout;
+  //    if (getLocalStorage(USER)) {
+  //       const {
+  //          accessToken: token,
+  //          refreshToken,
+  //          expiresIn: expiresInTime,
+  //       } = getLocalStorage(USER);
 
-         const now = new Date().getTime();
+  //       const now = new Date().getTime();
 
-         if (now < expiresInTime && token) {
-            dispatch(loginSuccess());
-            // console.log("EXPIRED");
-            timeout = setTimeout(() => {
-               dispatch(requestGetTokenOnExpired(refreshToken));
-            }, expiresInTime - now);
-         } else if (now >= expiresInTime) {
-            dispatch(requestGetTokenOnExpired(refreshToken));
-         }
+  //       if (now < expiresInTime && token) {
+  //          dispatch(loginSuccess());
+  //          // console.log("EXPIRED");
+  //          timeout = setTimeout(() => {
+  //             dispatch(requestGetTokenOnExpired(refreshToken));
+  //          }, expiresInTime - now);
+  //       } else if (now >= expiresInTime) {
+  //          dispatch(requestGetTokenOnExpired(refreshToken));
+  //       }
+  //    }
+
+  //    return () => {
+  //       clearTimeout(timeout);
+  //    };
+  // }, [dispatch]);
+
+  /**
+   * @implements getUserData
+   */
+  useEffect(() => {
+    const requestGetUserData = async () => {
+      try {
+        const userData = await userApi.getCurrentUser();
+        const { displayName, followers, id, images } = userData;
+        dispatch(
+          requestUserDataSuccess({
+            id,
+            displayName,
+            followers: followers.total,
+            image: images[0].url,
+          })
+        );
+        // console.log(userData);
+      } catch (error) {
+        requestUserDataFail({ error: error.message });
       }
+    };
+    if (!isLoggedIn && getLocalStorage(USER)) {
+      dispatch(loginSuccess());
+    } else if (isLoggedIn) {
+      requestGetUserData();
+    }
+  }, [dispatch, isLoggedIn]);
 
-      return () => {
-         clearTimeout(timeout);
-      };
-   }, [dispatch]);
-
-   /**
-    * @implements getUserData
-    */
-   useEffect(() => {
-      const requestGetUserData = async () => {
-         try {
-            const userData = await userApi.getCurrentUser();
-            const { displayName, followers, id, images } = userData;
-            dispatch(
-               requestUserDataSuccess({
-                  id,
-                  displayName,
-                  followers: followers.total,
-                  image: images[0].url,
-               })
-            );
-            // console.log(userData);
-         } catch (error) {
-            requestUserDataFail({ error: error.message });
-         }
-      };
-      if (isLoggedIn) {
-         requestGetUserData();
+  /**
+   * @implements check code response on URL
+   * @description check token after history.location.search change
+   *  - If not token then login = false, then check params response from spotify
+   *    when click to my-music page. code: success || error: fail
+   */
+  useEffect(() => {
+    const requestGetAuthorize = async () => {
+      const params = queryString.parse(history.location.search);
+      if (params?.error) {
+        dispatch(loginFail(params.error));
+      } else if (params?.code) {
+        // get authorize when has code from params response
+        const code = params.code;
+        let { data } = await authorizeApi.getAuthorize(code);
+        data = camelize(data);
+        dispatch(loginSuccess());
+        setLocalStorage(USER, {
+          accessToken: data.accessToken,
+          refreshToken: data.refreshToken,
+          expiresIn: new Date().getTime() + data.expiresIn * 1000,
+        });
       }
-   }, [dispatch, isLoggedIn]);
+    };
+    const token = getLocalStorage(USER)?.accessToken;
+    if (!token) {
+      requestGetAuthorize();
+    }
+  }, [dispatch, history.location.search]);
 
-   /**
-    * @implements check code response on URL
-    * @description check token after history.location.search change
-    *  - If not token then login = false, then check params response from spotify
-    *    when click to my-music page. code: success || error: fail
-    */
-   useEffect(() => {
-      const requestGetAuthorize = async () => {
-         const params = queryString.parse(history.location.search);
-         if (params?.error) {
-            dispatch(loginFail(params.error));
-         } else if (params?.code) {
-            // get authorize when has code from params response
-            const code = params.code;
-            let { data } = await authorizeApi.getAuthorize(code);
-            data = camelize(data);
-            dispatch(loginSuccess());
-            setLocalStorage(USER, {
-               accessToken: data.accessToken,
-               refreshToken: data.refreshToken,
-               expiresIn: new Date().getTime() + data.expiresIn * 1000,
-            });
-         }
-      };
-      const token = getLocalStorage(USER)?.accessToken;
-      if (!token) {
-         requestGetAuthorize();
-      }
-   }, [dispatch, history.location.search]);
-
-   // console.log(getLocalStorage(USER)?.accessToken);
-   // useEffect(() => {
-   //    const token =
-   //       getLocalStorage(USER)?.accessToken ||
-   //       getLocalStorage(PEPPERS).accessToken;
-   //    console.log(token);
-   // }, []);
-   return (
-      <PublicLayout>
-         <Suspense fallback={<div></div>}>
-            <Switch>
-               {routes.map(
-                  (route, index) =>
-                     route.layout === "PublicLayout" && (
-                        <PublicRoute {...route} key={index} />
-                     )
-               )}
-            </Switch>
-         </Suspense>
-      </PublicLayout>
-   );
+  // console.log(getLocalStorage(USER)?.accessToken);
+  // useEffect(() => {
+  //    const token =
+  //       getLocalStorage(USER)?.accessToken ||
+  //       getLocalStorage(PEPPERS).accessToken;
+  //    console.log(token);
+  // }, []);
+  return (
+    <PublicLayout>
+      <Suspense fallback={<div></div>}>
+        <Switch>
+          {routes.map(
+            (route, index) =>
+              route.layout === "PublicLayout" && (
+                <PublicRoute {...route} key={index} />
+              )
+          )}
+        </Switch>
+      </Suspense>
+    </PublicLayout>
+  );
 };
 
 export default App;
--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -4,47 +4,115 @@ import { PEPPERS, USER } from "constant/localStorage";
 import camelize from "camelize";
 import queryString from "query-string";
 import credentialApi from "./credentialApi";
+import authorizeApi from "./authorizeApi";
 const axiosClient = axios.create({
-   headers: {
-      "Content-Type": "application/json",
-   },
-   baseURL: "https://api.spotify.com/v1",
-   paramsSerializer: (params) => queryString.stringify(params),
+  headers: {
+    "Content-Type": "application/json",
+  },
+  baseURL: "https://api.spotify.com/v1",
+  paramsSerializer: (params) => queryString.stringify(params),
 });
 
 const getClientCredentialToken = async () => {
-   try {
-      let { data } = await credentialApi.getClientCredential();
-      // console.log(response?.data.access_token);
-      data = camelize(data);
-      setLocalStorage(PEPPERS, data);
-      return data.accessToken;
-   } catch (error) {
-      console.log(error);
-      return null;
-   }
+  try {
+    let { data } = await credentialApi.getClientCredential();
+    // console.log(response?.data.access_token);
+    data = camelize(data);
+    setLocalStorage(PEPPERS, data);
+    return data.accessToken;
+  } catch (error) {
+    // console.log(error);
+    return null;
+  }
+};
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error, token = null) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+  failedQueue = [];
 };
 
 axiosClient.interceptors.request.use(
-   async (config) => {
-      //using 2 token, one for not login, and one when login success
-      let token =
-         getLocalStorage(USER)?.accessToken ||
-         getLocalStorage(PEPPERS)?.accessToken;
-      if (!token) token = await getClientCredentialToken();
-      // if (token)
-      config.headers.Authorization = `Bearer ${token}`;
-      return config;
-   },
-   (error) => {}
+  async (config) => {
+    //using 2 token, one for not login, and one when login success
+    let token =
+      getLocalStorage(USER)?.accessToken ||
+      getLocalStorage(PEPPERS)?.accessToken;
+    if (!token) token = await getClientCredentialToken();
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => {}
 );
 
 axiosClient.interceptors.response.use(
-   (response) => {
-      // return response;
-      return camelize(response?.data);
-   },
-   (error) => {}
+  (response) => {
+    isRefreshing = false;
+    if (failedQueue.length > 0) {
+      const token = getLocalStorage(USER)?.accessToken;
+      processQueue(null, token);
+    }
+    // console.log("[AXIOS] - RESPONSE");
+    return camelize(response?.data);
+  },
+  async (error) => {
+    // console.log("[AXIOS] - ERROR, ", isRefreshing, error.config);
+
+    const status = error.response.status;
+    if (status === 401) {
+      return handle401Error(error);
+    }
+    //  console.dir(error);
+  }
 );
+async function handle401Error(error) {
+  // console.log("[AXIOS] - 401ERROR, ", isRefreshing);
+  const originalRequest = error.config;
+  // If is refreshing, push all failed requests into failedRequest
+  if (isRefreshing) {
+    // console.log("[AXIOS] - ANOTHER REQUEST COMING...");
+    return new Promise((resolve, reject) => {
+      failedQueue.push({ resolve, reject });
+    }).then(() => {
+      // console.log("[AXIOS] call failed request again, ", isRefreshing);
+      return axiosClient.request(originalRequest);
+    });
+  }
+  // if (!isRefreshing) {
+  // console.log("[AXIOS] REFRESHING...");
+
+  isRefreshing = true;
+  const lsUser = getLocalStorage(USER);
+  if (lsUser) {
+    const refreshToken = lsUser.refreshToken;
+    try {
+      let { data } = await authorizeApi.getTokenOnExpired(refreshToken);
+      data = camelize(data);
+      // console.log(data);
+      setLocalStorage(USER, {
+        accessToken: data.accessToken,
+        refreshToken,
+        expiresIn: data.expiresIn,
+      });
+      // isRefreshing = false;
+      // console.log("[AXIOS]: queue, ", failedQueue);
+      processQueue(null, null);
+      originalRequest.headers.Authorization = `Bearer ${data.accessToken}`;
+    } catch (error) {
+      throw Error("Refresh token failed! ", error?.message);
+    }
+  } else {
+    await getClientCredentialToken();
+  }
+  return axiosClient.request(originalRequest);
+  // }
+}
 
 export default axiosClient;
